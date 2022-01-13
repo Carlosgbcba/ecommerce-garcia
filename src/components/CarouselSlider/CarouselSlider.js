e7ef99099d246c461f6fb892ea7128f96e2af976
Carlosgbcba <60449127+Carlosgbcba@users.noreply.github.com>
2022-01-13 19:34:54 -0300

    Aplicando estilos al Navbar

--- src/components/CarouselSlider/CarouselSlider.js
+++ src/components/CarouselSlider/CarouselSlider.js
@@ -1,27 +1,24 @@
-import Container from 'react-bootstrap/esm/Container';
 import Carousel from 'react-bootstrap/Carousel'
-
-const loadImage = (imageName) => (require(`../../helpers/img/${imageName}`).default);
 
 export default function CarouselSlider() {
     return (
-        <Container>
+        <>
             <Carousel>
-                <Carousel.Item interval={2000}>
+                <Carousel.Item interval={3500}>
                     <img
                     className="d-block w-100"
-                    src={loadImage("logo2.png")}
-                    alt="Banner logo"
-                    />
-                </Carousel.Item>
-                <Carousel.Item interval={2000}>
-                    <img
-                    className="d-block w-100"
-                    src={loadImage("slider.png")}
+                    src={"https://firebasestorage.googleapis.com/v0/b/ecommerce-garcia.appspot.com/o/Carousel%2Fslider.png?alt=media&token=6b3eb9f7-3aa7-4652-9ab6-a45c33f9151d"}
                     alt="Banner estilo"
                     />
                 </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                    <img
+                    className="d-block w-100"
+                    src={"https://firebasestorage.googleapis.com/v0/b/ecommerce-garcia.appspot.com/o/Carousel%2Flogo2.png?alt=media&token=15eb3992-9e06-43be-969e-df60ffa46133"}
+                    alt="Banner logo"
+                    />
+                </Carousel.Item>
             </Carousel>
-        </Container>
+        </>
     )
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,22 +1,37 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import CartWidget from '../CartWidget/CartWidget'
+import './Navbar.css'
 
 export default function NavBar() {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{height:'4rem' }}>
+            <Navbar className="customNav" bg="light" style={{height:'157.5px'}}>
+                <Container style={{display: 'flex', justifyContent: 'center'}}>
+                <Navbar.Brand href="/">
+                    <div>
+                        <img
+                            src="https://firebasestorage.googleapis.com/v0/b/ecommerce-garcia.appspot.com/o/Carousel%2Flogo1.png?alt=media&token=8491f2f8-9b6c-4a14-a36e-9a8023410fba"
+                            width="425.5"
+                            height="157.5"
+                            className="d-inline-block align-center"
+                            alt="SELECTO CLOTHES"
+                        />
+                    </div>
+                </Navbar.Brand>
+                </Container>
+            </Navbar>
+            <Navbar className="customNav" collapseOnSelect expand="lg" bg="light" variant="light" style={{height:'4rem' }}>
                 <Container>
-                <Navbar.Brand href="/">SELECTO CLOTHES</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <NavDropdown title="Categorias" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="/categoria/remeras" >Remeras</NavDropdown.Item>
-                            <NavDropdown.Item href="/categoria/pantalones" >Pantalones</NavDropdown.Item>
-                        </NavDropdown>
+                    <Nav>
+                        <Nav.Link href="/" style={{fontSize: "20px", color: "black"}}>Inicio</Nav.Link>
+                    </Nav>
+                    <Nav className="m-auto">
+                        <Nav.Link href="/categoria/remeras" style={{marginLeft: "30px", marginRight: "30px", fontSize: "20px", color: "black"}} >Remeras</Nav.Link>
+                        <Nav.Link href="/categoria/pantalones" style={{marginLeft: "30px", marginRight: "30px", fontSize: "20px", color: "black"}} >Pantalones</Nav.Link>
                     </Nav>
                     <Nav>
                         <CartWidget />
