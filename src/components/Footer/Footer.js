a7a8b6e4d73cac5c1e5cc6809212abe3a3c99f9f
Carlosgbcba <60449127+Carlosgbcba@users.noreply.github.com>
2022-01-24 14:20:15 -0300

    Version Final

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,6 @@
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import './Footer.css'
 
 export default function Footer() {
     return (
@@ -5,6 +8,18 @@ export default function Footer() {
             <br />
             <center>
                 &copy; Copyright SELECTO CLOTHES
+                <br />
+                <br />
+
+                <a href="https://www.linkedin.com/in/carlos-garcia-barrera-4b729219a/" className="link" target="_blank" rel="noreferrer">
+                    <FaLinkedin style={{ fontSize: "25px",  marginRight: "10px" }} /> 
+                </a>
+
+                <a href="https://github.com/Carlosgbcba/ecommerce-garcia" className="link" target="_blank" rel="noreferrer">
+                    <FaGithub style={{ fontSize: "25px",  marginLeft: "10px" }} /> 
+                </a>
+
+
             </center>
             <br />
         </footer>
